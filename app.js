0a9a8ec2cee35482a4b1671d4560918d1503ffc8
Gina Pultorak <gina.pultorak@gmail.com>
2019-12-10 20:58:35 -0800

    add City constructor function

--- app.js
+++ app.js
@@ -2,9 +2,26 @@
 
 console.log('js connected!');
 
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm']
+
 var header = ['Name', 1, 2, 3, 'Name Totals'];
 var body = [['Body1', 1, 2, 3, 4],['Body2', 1, 2, 3, 4],['Body3', 1, 2, 3, 4]];
 var footer = ['Totals', 3, 4, 5, 'Grand Total'];
+
+function City (name, minCust, maxCust, avg) {
+  this.name = name;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.avg = avg;
+  this.avgCookiesPerHour = [];
+}
+
+var seattle = new City('Seattle', 5, 10, 2.6);
+var tokyo = new City('Tokyo', 10, 20, 3.4);
+var philly = new City('Philadelphia', 20, 30, 8.2);
+
+var allCities = [seattle, tokyo, philly];
+
 
 // get table holder and render the thead, tbody, and tfoot
 var tableHolder = document.getElementById('table-holder');
